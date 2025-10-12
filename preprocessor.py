3eb37dea3da292326f19a91171778b5383b35022
Yechang618 <yechang618@gmail.com>
2025-10-12 23:21:07 +0800

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -6,12 +6,14 @@ import os
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-v0_8')
 
-def generate_df(symbols):
+def generate_df(symbols, dataset_name):
     df = None
     address = 'datasets'
-    spot_name1 = 'binance_book_snapshot_5_2025-05-22_'
+    # spot_name1 = 'binance_book_snapshot_5_2025-05-22_'
+    # swap_name1 = 'binance-futures_book_snapshot_5_2025-05-22_'
+    spot_name1 = 'binance_' + dataset_name +'_'
+    swap_name1 = 'binance-futures_' + dataset_name +'_'  
     spot_name2 = '.csv.gz'
-    swap_name1 = 'binance-futures_book_snapshot_5_2025-05-22_'
     swap_name2 = '.csv.gz'
     for symbol in symbols:
         print(f"Processing symbol: {symbol}")
@@ -26,9 +28,13 @@ def generate_df(symbols):
         # print(df_swap.info())
         df_spot = df_spot.set_index(pd.DatetimeIndex(df_spot['local_timestamp']))
         df_swap = df_swap.set_index(pd.DatetimeIndex(df_swap['local_timestamp']))
-        df_swap_qt = pd.DataFrame({'swap_bids': df_swap['bids[0].price'], 'swap_asks': df_swap['asks[0].price']}).drop_duplicates()
-        df_spot_qt = pd.DataFrame({'spot_bids': df_spot['bids[0].price'], 'spot_asks': df_spot['asks[0].price']}).drop_duplicates()
+        # df_swap_qt = pd.DataFrame({'swap_bids': df_swap['bids[0].price'], 'swap_asks': df_swap['asks[0].price']}).drop_duplicates()
+        # df_spot_qt = pd.DataFrame({'spot_bids': df_spot['bids[0].price'], 'spot_asks': df_spot['asks[0].price']}).drop_duplicates()
+        df_swap_qt = pd.DataFrame({'swap_bids': df_swap['bid_price'], 'swap_asks': df_swap['ask_price']}).drop_duplicates()
+        df_spot_qt = pd.DataFrame({'spot_bids': df_spot['bid_price'], 'spot_asks': df_spot['ask_price']}).drop_duplicates()
         # Resample df_spot_qt and df_swap_qt to a higher frequency (e.g., 1ms) to allow for finer matching
+        df_spot_qt = df_spot_qt[~df_spot_qt.index.duplicated(keep='first')]
+        df_swap_qt = df_swap_qt[~df_swap_qt.index.duplicated(keep='first')]
         df_spot_qt_resampled = df_spot_qt.resample('1s').ffill()
         df_swap_qt_resampled = df_swap_qt.resample('1s').ffill()  
         # Merge the resampled dataframes based on the closest time index within a tolerance
@@ -83,8 +89,10 @@ if __name__ == "__main__":
                "LTCUSDT","ETCUSDT","FORMUSDT",
                "TONUSDT","HFTUSDT","DOTUSDT",
                "CHESSUSDT","MKRUSDT","WIFUSDT"]
-    df = generate_df(symbols)
+    dataset_name = 'quotes_2025-06-07'
+    # dataset_name = 'book_snapshot_5_2025-05-22'
+    df = generate_df(symbols, dataset_name)
     print(df.head(5))
     print(df.info())
-    df.to_csv('basis_5_2025-05-22.csv', index=True, header=True, quoting=csv.QUOTE_NONNUMERIC)
-    print("DataFrame saved to 'basis_5_2025-05-22.csv'")
+    df.to_csv('basis_' + dataset_name + '.csv', index=True, header=True, quoting=csv.QUOTE_NONNUMERIC)
+    print("DataFrame saved to 'basis_" + dataset_name + ".csv'  successfully.")
